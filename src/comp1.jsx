74bf4697876e0f2d04c9028d8fababe0d810ee10
vmanasa123 <111417202+vmanasa123@users.noreply.github.com>
2023-01-18 14:49:31 +0530

    Add files via upload

---
+++ src/comp1.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Component1 = () => {
+  return (
+    <div id="box1">
+      <h2>This is created using Functional Component</h2><br/>
+    <h3>This is done using external CSS</h3><br/>
+    <h3 style={{color:'blue'}}>This is done using Inline CSS</h3> 
+
+    </div>
+  )
+}
+
+export default Component1
+
+
---
+++ src/comp2.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Component2 extends Component {
+  render() {
+    return (
+        <div id="box2"><h2>This is created using Class Component</h2><br/>
+        <h3>This is done using external CSS</h3><br/>
+        <h3 style={{color:'blue'}}>This is done using Inline CSS</h3> 
+    
+        </div>
+    )
+  }
+}
